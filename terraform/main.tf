7fd31d1a4ac13fba82ec4781989b7368272ffbd6
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2024-03-20 23:04:53 +0530

    jenkins

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 module "catalogue" {
     source = "git::https://github.com/sridevi2009/terraform-roboshop-app.git?ref=main"
     vpc_id = data.aws_ssm_parameter.vpc_id.value
-    component_sg_id = data.aws_ssm_parameter.user_sg_id.value
+    component_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
     private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value) #list of private subnet ids
     iam_instance_profile = var.iam_instance_profile
     project_name = var.project_name
@@ -12,5 +12,6 @@ module "catalogue" {
     app_alb_listener_arn = data.aws_ssm_parameter.app_alb_listener_arn.value
     rule_priority = 30
     app_version = var.app_version
+    
 
 }
